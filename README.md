16044a36397ed5fab652b7f489b137372ac4f2cc
Prince Hlela <mphdigital@protonmail.com>
2025-05-26 20:05:44 +0200

    resolved the identification issues

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
  <!-- to create html elements and append then to the todos container -->
 
- > Add the click event on submit
- >
+Problem #1
+
+after creating the todo elements dynamically, i faced the identity challenge when pressing buttons i need a dynamically generated method of identifying the element the event listener is attached to and #check, #edit or #delete 
--- app.js
+++ app.js
@@ -3,82 +3,106 @@ const submitBtn = document.querySelector(".submit");
 const inputEl = document.querySelector(".input-el")
 const toDoContainer = document.querySelector(".todo-container")
 
+let idIndex = 0;
+
 // event listners
 submitBtn.addEventListener("click", submitToDo);
 
 // FUNCTIONS
 
-function submitToDo(event){
-    event.preventDefault();
-   
+function submitToDo(e){
+    e.preventDefault();
     const value = inputEl.value;
 
     if (value){
         // created a todo element and added a class "todo".
-    const todo = document.createElement("div");
-    todo.classList.add("todo")
+        const todo = document.createElement("div");
+        todo.classList.add("todo");
+        todo.setAttribute("id", `id${idIndex}`)
 
-    // create a button and add the class "check"
-    const checkBtn = document.createElement("button");
-    checkBtn.classList.add("check");
-    checkBtn.addEventListener("click", checkToDo)
+        // create a button and add the class "check"
+        const checkBtn = document.createElement("button");
+        checkBtn.classList.add("check");
+        checkBtn.addEventListener("click", (event) => {
 
-    // create the image check icon and set src and alt attributes
-    const checkImg = document.createElement("img");
-    checkImg.setAttribute("src", "images/check.svg");
-    checkImg.setAttribute("alt", "check icon");
+            event.preventDefault();
+            const parentContainer = event.target.parentElement.parentElement;
+            // console.log(parentContainer)
+            const id = parentContainer.id;
 
-    //append the check img to the check button element
-    checkBtn.appendChild(checkImg);
+            // if the id of the todo container is equal to the current ID and the todo container does not contain the checked class add the check class to the todo container otherwise remove it.
+            if(`id${idIndex} === id` && !parentContainer.classList.contains("checked")){
+                parentContainer.classList.add("checked");
+            }else{
+                parentContainer.classList.remove("checked");
+            }
 
-    // create the input and set the value and type attributes and disable the element
-    const todoInput = document.createElement("input");
-    todoInput.setAttribute("value", `${value}`);
-    todoInput.setAttribute("type", "text");
-    todoInput.disabled = true;
-    todoInput.classList.add("todo-display");
+        })
 
-    // create the button container, the buttons and their images.
-    const buttonContainer = document.createElement("div");
-    buttonContainer.classList.add("buttons");
+        // create the image check icon and set src and alt attributes
+        const checkImg = document.createElement("img");
+        checkImg.setAttribute("src", "images/check.svg");
+        checkImg.setAttribute("alt", "check icon");
 
-    // >> create the edit button
-    const editToDoBtn = document.createElement("button");
-    editToDoBtn.classList.add("edit")
-    editToDoBtn.addEventListener("click", editToDo)
-    
-    const editImg = document.createElement("img");
-    editImg.setAttribute("src", "images/edit.svg");
-    editImg.setAttribute("alt", "edit icon");
+        //append the check img to the check button element
+        checkBtn.appendChild(checkImg);
 
-    editToDoBtn.appendChild(editImg);
+        // create the input and set the value and type attributes and disable the element
+        const todoInput = document.createElement("input");
+        todoInput.setAttribute("value", `${value}`);
+        todoInput.setAttribute("type", "text");
+        todoInput.disabled = true;
+        todoInput.classList.add("todo-display");
 
-    // >> create the delete button
-    const deleteToDoBtn = document.createElement("button");
-    deleteToDoBtn.classList.add("delete");
-    deleteToDoBtn.addEventListener("click", deleteToDo)
-    
-    const deleteImg = document.createElement("img");
-    deleteImg.setAttribute("src", "images/delete.svg");
-    deleteImg.setAttribute("alt", "delete icon");
+        // create the button container, the buttons and their images.
+        const buttonContainer = document.createElement("div");
+        buttonContainer.classList.add("buttons");
 
-    deleteToDoBtn.appendChild(deleteImg);
+        // >> create the edit button
+        const editToDoBtn = document.createElement("button");
+        editToDoBtn.classList.add("edit")
+        editToDoBtn.addEventListener("click", editToDo)
+        
+        const editImg = document.createElement("img");
+        editImg.setAttribute("src", "images/edit.svg");
+        editImg.setAttribute("alt", "edit icon");
 
-    // append the edit and delete buttons to the buttons container
-    buttonContainer.appendChild(editToDoBtn);
-    buttonContainer.appendChild(deleteToDoBtn);
+        editToDoBtn.appendChild(editImg);
 
-    // append the check button, todo input and the buttons to the todo element
-    todo.appendChild(checkBtn);
-    todo.appendChild(todoInput);
-    todo.appendChild(buttonContainer);
+        // >> create the delete button
+        const deleteToDoBtn = document.createElement("button");
+        deleteToDoBtn.classList.add("delete");
+        deleteToDoBtn.addEventListener("click", (event) => {
 
-    // append the todo element into the todo container.
-    toDoContainer.appendChild(todo)
-    console.log(todo);
+            const id = event.target.parentElement.parentElement.parentElement.id;
 
-    // reset the input value of the input element
-    inputEl.value = "";
+            if(`id${idIndex} === id`){
+                event.target.parentElement.parentElement.parentElement.remove();
+            }
+        })
+        
+        const deleteImg = document.createElement("img");
+        deleteImg.setAttribute("src", "images/delete.svg");
+        deleteImg.setAttribute("alt", "delete icon");
+
+        deleteToDoBtn.appendChild(deleteImg);
+
+        // append the edit and delete buttons to the buttons container
+        buttonContainer.appendChild(editToDoBtn);
+        buttonContainer.appendChild(deleteToDoBtn);
+
+        // append the check button, todo input and the buttons to the todo element
+        todo.appendChild(checkBtn);
+        todo.appendChild(todoInput);
+        todo.appendChild(buttonContainer);
+
+        // append the todo element into the todo container.
+        toDoContainer.appendChild(todo)
+        // console.log(todo);
+
+        // reset the input value of the input element
+        inputEl.value = "";
+        idIndex++;
     }else{
         alert("input empty, write your")
     }
@@ -86,18 +110,15 @@ function submitToDo(event){
 }
 
 // check todo fn
-const checkToDo = () => {
-    // alert("check button pressed")
-
-    // when you press the check button the class of checked will be added to the todo element.
-    document.querySelector(".todo").classList.add("checked")
-}
+// const checkToDo = () => {
+//     document.querySelector(".todo").classList.add("checked")
+// }
 
 // Delete todo fn
-const deleteToDo = () => {
-    alert("todo deleted")
-}
-// Edit todo fn
+// const deleteToDo = (event, id) => {
+//     // alert("todo deleted")
+// }
+// Edits todo fn
 const editToDo = () => {
     
     // changing the disabled to false when the edit button is pressed.
